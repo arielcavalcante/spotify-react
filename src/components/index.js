fb71741f7e209a90ca00d86d55a061b7f827d6e5
Ariel <ariel.cavalcante@edu.unifor.br>
2021-09-21 20:50:42 -0300

    WIP - footer

--- src/components/index.js
+++ src/components/index.js
@@ -2,9 +2,20 @@ import Accordion from './accordion/Accordion';
 import Button from './button/Button';
 import Card from './card/Card';
 import CardList from './cardlist/CardList';
+import Footer from './footer/Footer';
 import ListItem from './listitem/ListItem';
 import Menu from './menu/Menu';
 import NavBar from './navbar/NavBar';
 import Section from './section/Section';
 
-export { Accordion, Button, Card, CardList, ListItem, Menu, NavBar, Section };
+export {
+	Accordion,
+	Button,
+	Card,
+	CardList,
+	Footer,
+	ListItem,
+	Menu,
+	NavBar,
+	Section,
+};
