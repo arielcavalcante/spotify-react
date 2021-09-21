1c8cbefcffae91bce05e5a266f0eb4d5058a4d0a
Rayane Santos <rayanesantos-131@hotmail.com>
2021-09-20 22:58:53 -0300

    fixing accordion state

--- src/components/accordion/Accordion.js
+++ src/components/accordion/Accordion.js
@@ -4,19 +4,24 @@ import { v4 as uuid } from 'uuid';
 import { ListItem } from '../';
 import './Accordion.css';
 
-export default function Accordion({ list }) {
-	const [active, setActive] = useState(false);
+export default function Accordion({ list = []}) {
+	const [data, setData] = useState(list);
 
+	function setItemActive(i, active){
+		let newList = [...data]
+		newList[i]['active'] = active
+		setData(newList)
+	}
 	return (
 		<div className='accordion-wrapper'>
-			{list.map((l, i) => {
+			{data.map((l, i) => {
 				return (
 					<div className='accordion-component'>
-						<h3 className='category__title' onClick={() => setActive(!active)}>
+						<h3 className='category__title' onClick={() => setItemActive(i, !l['active'])}>
 							{l.title}
 						</h3>
 						<div
-							className={`accordion__list ${active ? 'active' : 'hide'}`}
+							className={`accordion__list ${l['active'] ? 'active' : 'hide'}`}
 							key={uuid()}
 						>
 							<ListItem list={l.items} />
