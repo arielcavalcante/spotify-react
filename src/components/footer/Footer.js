e596d53a8931fd52c854e1800476367d37e06425
Ariel <37940722+arielcavalcante@users.noreply.github.com>
2021-09-22 20:57:57 -0300

    fix svgs in footer social networks

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -3,48 +3,41 @@ import { Link } from 'react-router-dom';
 import { v4 as uuid } from 'uuid';
 
 import './Footer.css';
-import { logo } from '../../icons';
+import { logo, ig, fb, tt } from '../../icons';
 
 export default function Footer({ footerlist }) {
-	const data = new Date();
-
 	return (
-		<footer>
+		<footer className='footer-component'>
 			<nav>
 				<div className='col'>
 					<Link className='logo' to='/faq'>
 						{logo}
-						<p>{data.toLocaleDateString()}</p>
 					</Link>
 				</div>
-				{footerlist.map((f, i) => {
-					return (
-						<div className='col' key={uuid()}>
-							<h3 className='category__smallcaps'>{f.title}</h3>
-							<ul>
-								<li>
-									{f.li.map((li, y) => {
-										return (
-											<Link className='menu__link' to='/faq'>
-												{li}
-											</Link>
-										);
-									})}
-								</li>
-							</ul>
-						</div>
-					);
-				})}
+				<div className='footer__menu'>
+					{footerlist.map((f, i) => {
+						return (
+							<div className='col' key={uuid()}>
+								<h3 className='category__smallcaps'>{f.title}</h3>
+								<ul>
+									<li>
+										{f.li.map((li, y) => {
+											return (
+												<Link className='menu__link' to='/faq'>
+													{li}
+												</Link>
+											);
+										})}
+									</li>
+								</ul>
+							</div>
+						);
+					})}
+				</div>
 				<div className='col socialmedia'>
-					<Link to='/faq'>
-						<span className='socialmedia insta'></span>
-					</Link>
-					<Link to='/faq'>
-						<span className='socialmedia twitter'></span>
-					</Link>
-					<Link to='/faq'>
-						<span className='socialmedia face'></span>
-					</Link>
+					<Link to='/faq'>{ig}</Link>
+					<Link to='/faq'>{tt}</Link>
+					<Link to='/faq'>{fb}</Link>
 				</div>
 			</nav>
 		</footer>
