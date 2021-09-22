e406d1c35d59f7610861def4be35fa05d4d96e7b
Ariel <ariel.cavalcante@edu.unifor.br>
2021-09-21 21:47:22 -0300

    adds data

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -6,12 +6,15 @@ import './Footer.css';
 import { logo } from '../../icons';
 
 export default function Footer({ footerlist }) {
+	const data = new Date();
+
 	return (
 		<footer>
 			<nav>
 				<div className='col'>
 					<Link className='logo' to='/faq'>
 						{logo}
+						<p>{data.toLocaleDateString()}</p>
 					</Link>
 				</div>
 				{footerlist.map((f, i) => {
@@ -32,6 +35,17 @@ export default function Footer({ footerlist }) {
 						</div>
 					);
 				})}
+				<div className='col socialmedia'>
+					<Link to='/faq'>
+						<span className='socialmedia insta'></span>
+					</Link>
+					<Link to='/faq'>
+						<span className='socialmedia twitter'></span>
+					</Link>
+					<Link to='/faq'>
+						<span className='socialmedia face'></span>
+					</Link>
+				</div>
 			</nav>
 		</footer>
 	);
