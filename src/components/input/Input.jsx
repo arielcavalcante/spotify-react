65a7a1287faed88be983c61030bcd0662e8ade88
Ariel <37940722+arielcavalcante@users.noreply.github.com>
2021-10-26 14:13:27 -0300

    Add login pagewith minor tweaks in button and input components for improved reusability

--- src/components/input/Input.jsx
+++ src/components/input/Input.jsx
@@ -2,18 +2,46 @@ import React from 'react';
 
 import './Input.css';
 
-export default function Input({placeholder = '', value='', onChange, onBlur, type='text', name, error=false, errorText='', fullWidth}) {
+export default function Input({
+	placeholder = '',
+	value = '',
+	onChange,
+	onBlur,
+	type = 'text',
+	name,
+	label,
+	label2,
+	aria,
+	error = false,
+	errorText = '',
+	fullWidth,
+	autofocus,
+	autocapitalize,
+	autocomplete,
+	autocorrect,
+	required = 'required',
+}) {
 	return (
 		<div className='input-container'>
-        <input className={`custom-input ${fullWidth ? 'w-full' : ''}`}
-				placeholder={placeholder} 
+			<label for={name}>{label}</label>
+			<input
+				className={`custom-input ${fullWidth ? 'w-full' : ''}`}
+				placeholder={placeholder}
 				type={type}
 				id={name}
 				name={name}
 				value={value}
 				onChange={onChange}
-				onBlur={onBlur ? onBlur : null}/>
-		{error && <span>{errorText}</span>}
+				onBlur={onBlur ? onBlur : null}
+				aria-label={aria}
+				autofocus={autofocus}
+				autocapitalize={autocapitalize}
+				required={required}
+				autocomplete={autocomplete}
+				autocorrect={autocorrect}
+			/>
+			<label for={name}>{label2}</label>
+			{error && <span>{errorText}</span>}
 		</div>
 	);
 }
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,9 +1,10 @@
 import Faq from './faq/Faq';
 import Home from './home/Home';
+import Login from './login/Login';
 import Player from './player/Player';
 import PlaylistCollection from './playlistcollection/PlaylistCollection';
 
 import Playlist from './playlist/Playlist';
 import Signup from './signup/Signup';
 
-export { Faq, Home, Player, PlaylistCollection, Playlist, Signup };
+export { Faq, Home, Login, Player, PlaylistCollection, Playlist, Signup };
